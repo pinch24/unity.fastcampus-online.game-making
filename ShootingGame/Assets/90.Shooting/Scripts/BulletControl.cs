f6076e6550b5b528f398feb3da3a0fb42731b998
mk <pinch24@gmail.com>
2019-11-28 15:32:24 +0900

    06.06 슈팅게임

---
+++ ShootingGame/Assets/90.Shooting/Scripts/BulletControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+	public float BulletSpeed = 100.0f;
+	private Transform myTransform = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveAmount = BulletSpeed * Vector3.up * Time.deltaTime;
+		myTransform.Translate(moveAmount);
+
+		// 탄환이 화면 밖으로 나갔으면,
+		if (myTransform.position.y > 60.0f) {
+
+			// 탄환을 제거한다.
+			Destroy(gameObject); // gameObject: Bullet Prefab
+		}
+    }
+}
--- ShootingGame/Assets/90.Shooting/Scripts/PlayerControl.cs
+++ ShootingGame/Assets/90.Shooting/Scripts/PlayerControl.cs
@@ -20,6 +20,12 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		// -1 ~ 1: 왼쪽 화살키(-1), 오른쪽 화살키(1)
+        float axis = Input.GetAxis("Horizontal");
+		// Debug.Log("AXID: " + axis);
+
+		// 프레임 당 이 게임 오브젝트가 이동하는 속도와 방향의 양
+		Vector3 moveAmount = axis * Speed * -Vector3.right * Time.deltaTime;
+		myTransform.Translate(moveAmount);
     }
 }
