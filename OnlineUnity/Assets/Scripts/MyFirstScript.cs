cfc8760b4cb83168f89d2b852cc8006e6a77e37c
mk <pinch24@gmail.com>
2019-11-28 12:33:20 +0900

    07.07 스크립팅 기초 - 3

---
+++ OnlineUnity/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake Called.");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Called.");
+        Debug.Log("Hello, World.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogWarning("Update Called.");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("LateUpdate Called.");
+    }
+}
